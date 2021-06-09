f73cf263ef970c7f280d9dbb5cfc144afec70cc2
pusalieth <johnpring09@gmail.com>
2021-06-09 15:03:19 -0700

    -- update - first working condition

--- gui.py
+++ gui.py
@@ -1,104 +1,121 @@
-from tkinter import Tk, Label, Text, Button, filedialog, ttk, Frame, messagebox
+from tkinter import Tk, Label, Button, filedialog, ttk, Frame, messagebox
 import subprocess
 import threading
-from os import path, listdir
+import sys
+import os
 
-def processControl(option, path):
-    if(option == 'create'):
-        subprocess.Popen('extract-xiso.exe -c "%s"' % path)
-    elif(option == 'extract'):
-        subprocess.Popen('extract-xiso.exe -x "%s"' % path)
+# TO-DO
+# output directory same or different
+# Progress bar
+# Display filename of selected file object
 
-def threadedProcess():
-        threading.Thread(target=processControl).start()
 
-def getPath(recursive=False):
-    file_path = filedialog.askdirectory()
-    if(path.isfile(file_path)):
-        # attempt to extract file
-        try:
-            filename, extension = str(file_path).split('.')
-            if(extension == 'iso'):
-                # need to extract
-                return 'extract'
-            else:
-                # error, file not supported
-                messagebox.showerror('File type is not supported.\nMust be iso filetype.')
-        except:
-            # error, file has no extension
-            messagebox.showerror('File object has no extension')
-    elif(path.isdir(file_path)):
-        if(not recursive):
-            # compress directory
-            file_objects = listdir()
-            found = False
-            for file in file_objects:
-                if('.xbe' in file):
-                    found = True
-                    break
-            
-            if(found):
-                return 'create'
-            else:
-                messagebox.showerror('Directory is not an xbe directory')
+class GUI:
+    option = ''
+    file_path = ''
+    filename_label = None
+    process_label = None
+    filename_text = 'File Selected: '
+    process_text = 'Process: '
+    output_directory = './'
+
+    def __init__(self):
+        self.exe_directory = os.path.join("%s/exe/" % os.getcwd())
+        self.createWindow()
+
+    def processControl(self):
+        if(self.option == 'create'):
+            subprocess.Popen('%s/extract-xiso.exe -c "%s" "%s.iso"' % (self.exe_directory, os.path.join(
+                os.path.dirname(self.file_path)), os.path.join(self.output_directory, self.file_object_name)))
+        elif(self.option == 'extract'):
+            subprocess.Popen('%s/extract-xiso.exe -x "%s"' %
+                             (self.exe_directory, os.path.join(self.file_path)))
         else:
-            # create from all viable sub directories
-            pass
-    else:
-        # error, wth file object is this?
-        messagebox.showerror('Completely unknown filetype.')
+            messagebox.showerror('No File selected')
 
-def createWindow():
-    window = Tk()
-    window.title("pyXISO")
+    def threadedProcess(self):
+        threading.Thread(target=self.processControl).start()
 
-    # Frames
-    topframe = Frame(window)
-    bottomframe = Frame(window)
-    topframe.grid(column=0, row=0)
-    bottomframe.grid(column=0, row=1)
+    def getPath(self, recursive=False):
+        # get selection from user
+        self.file_path = filedialog.askopenfilename()
+        try:
+            temp, extension = str(self.file_path).split('.')
+        except:
+            messagebox.showerror('Error', 'File has no extension')
 
-    # File/Directory name
-    text_width = 100
-    file_Label = Label(topframe, text="File Selected: None")
-    file_Label.grid(column=0, row=0, padx=20)
+        if(extension == 'xbe'):
+            self.option = 'create'
+            self.file_object_name = str(
+                os.path.dirname(self.file_path)).split('/')[-1]
+            print(self.file_object_name)
+        elif(extension == 'iso'):
+            self.option = 'extract'
+            self.file_object_name = str(self.file_path).split('/')[-1]
+        else:
+            messagebox.showerror('Error', 'Incomaptible file type')
 
-    path_btn = Button(topframe, text="Select the file or directory", command=getPath)
-    path_btn.grid(column=1, row=0, padx=20)
+        try:
+            self.filename_label.configure(text="%s%s" % (
+                self.filename_text, self.file_object_name))
+            self.process_label.configure(text="%s%s" % (
+                self.process_text, str(self.option).upper()))
+        except:
+            messagebox.showerror("Error", "Don't do that...")
 
-    start_btn = Button(topframe, text="Start", command=threadedProcess)
-    start_btn.grid(column=2, row=0, padx=20)
+    def setOutputDirectory(self):
+        self.output_directory = filedialog.askdirectory()
 
-    # Progress bar
-    style = ttk.Style()
-    style.theme_use('default')
-    style.configure("black.Horizontal.TProgressbar", background='black')
-    bar = ttk.Progressbar(bottomframe, length=text_width*6,
-                        style='black.Horizontal.TProgressbar')
-    bar.grid(column=0, row=1, padx=4, pady=4)
+    def createWindow(self):
+        window = Tk()
+        window.title("pyXISO")
 
-    # def update():
-    #     bar['value'] = progress_value
-    #     if(progress_value >= 100):
-    #         selected_class = var.get()
-    #         global media_label
-    #         res = 'Finished! %s Download, %s' % (
-    #             selected_class, media_label)
-    #         URL_Label.configure(text=res)
-    #     elif(progress_value > 0):
-    #         global playlist_checked
-    #         if(playlist_checked):
-    #             global playlist_index
-    #             global playlist_count
-    #             playlist_label.configure(
-    #                 text="%s/%s" % (str(playlist_index + 1), str(playlist_count)))
-    #         selected_class = var.get()
-    #         URL_Label.configure(
-    #             text=str("Downloading...  %s %s" % (selected_class, media_label)))
-    #     window.after(100, update)
+        # Frames
+        topframe = Frame(window)
+        middleframe = Frame(window)
+        bottomframe = Frame(window)
+        topframe.grid(column=0, row=0)
+        middleframe.grid(column=0, row=1)
+        bottomframe.grid(column=0, row=2)
 
-    # Execute GUI
-    #threading.Thread(target=update).start()
-    window.mainloop()
-    
-createWindow()
+        # File/Directory name
+        text_width = 100
+        self.filename_label = Label(
+            topframe, text="%sNone" % self.filename_text)
+        self.filename_label.grid(column=0, row=0, padx=20)
+
+        # Process detected
+        text_width = 100
+        self.process_label = Label(topframe, text="%sNone" % self.process_text)
+        self.process_label.grid(column=2, row=0, padx=6)
+
+        # Begin selected process
+        start_btn = Button(topframe, text="Start",
+                           command=self.threadedProcess)
+        start_btn.grid(column=4, row=0, padx=[4, 4])
+
+        # Select output directory
+        output_btn = Button(
+            middleframe, text="Select output directory", command=self.setOutputDirectory)
+        output_btn.grid(column=3, row=0, padx=[20, 0])
+
+        # Get working directory
+        path_btn = Button(
+            middleframe, text="Select the file or directory", command=self.getPath)
+        path_btn.grid(column=1, row=0, padx=[20, 0])
+
+        # Progress bar
+        style = ttk.Style()
+        style.theme_use('default')
+        style.configure("black.Horizontal.TProgressbar", background='black')
+        bar = ttk.Progressbar(bottomframe, length=text_width*6,
+                              style='black.Horizontal.TProgressbar')
+        bar.grid(column=0, row=1, padx=4, pady=4)
+
+        # Execute GUI
+        # threading.Thread(target=update).start()
+        window.mainloop()
+
+
+if __name__ == "__main__":
+    GUI()
